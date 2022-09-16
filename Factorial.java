d6b264e46fed0ee76908965c0cdab05a3220894b
Omkar1896 <omkar18.rao@gmail.com>
2022-09-16 10:20:30 -0700

    changes

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package JavaPrograms;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		int no=10;
+		int fact=1;
+		
+		for(int i=1;i<=10;i++)
+		{
+			fact=fact*i;
+		Omkar @12:00
+		// in git
+		}
+		
+		System.out.println("factorial of "+no+" is" +fact);
+	}
+
+}
